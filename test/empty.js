083de5bc29791565498fcf577e942002ffb298c0
Diego Alfonso <diego.alfonso2@gmail.com>
2017-10-26 12:49:45 -0300

    Initial commit

---
+++ test/empty.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var chai = require('chai');
+var should = chai.should();
+
+describe('Prueba vacia', function() {
+  it('Deberia pasar siempre', function() {
+    true.should.equal(true);
+  });
+});
