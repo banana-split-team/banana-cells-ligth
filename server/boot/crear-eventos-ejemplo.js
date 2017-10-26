770cd37b727880f26b1d2a09369d3fe072326926
Diego Alfonso <diego.alfonso2@gmail.com>
2017-10-26 15:11:42 -0300

    Prueba unitaria de /Eventos

---
+++ server/boot/crear-eventos-ejemplo.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = function(app) {
+  // data sources
+  var db = app.dataSources.db;
+
+  db.automigrate(['Evento', 'Teatro'], function(err) {
+    if (err) throw err;
+    var Teatro = app.models.Teatro;
+    var Evento = app.models.Evento;
+    const teatros = [
+      {
+        'nombre': 'SAN MARTIN',
+        'direccion': 'CORRIENTES AV. 1530',
+        'geolocalizacion': {
+          'lat': -58.38864181184383,
+          'lng': -34.604238054156944,
+        },
+        'id': 1,
+      },
+    ];
+    var resultadoTeatros = Teatro.create(teatros, function(err) {
+      if (err) throw err;
+      console.log('Modelos creados: Teatro');
+    });
+    console.log('TeatroId: ' + resultadoTeatros[0].id);
+    const eventos = [
+      {
+        'nombre': 'El fantasma de la Opera',
+        'director': 'Pepe Cibrian',
+        'precios': [
+          150,
+        ],
+        'funciones': [
+          '2017-10-26T21:30:00.000Z',
+        ],
+        'sinopsis': "El fantasma de la ópera (Le Fantôme de l'Opéra) es una...",
+        'teatroId': resultadoTeatros[0].id,
+      },
+    ];
+    Evento.create(eventos, function(err) {
+      if (err) throw err;
+      console.log('Modelos creados: Evento');
+    });
+  });
+};
