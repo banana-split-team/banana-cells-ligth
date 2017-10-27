5beb2f314be303b92fdebe23974e1fc3aaefbdf9
Nahuel <alvarez.nahuel@gmail.com>
2017-10-27 10:53:22 -0300

    Se incluye la logica en los eventos de descuentos.

--- common/models/descuento.js
+++ common/models/descuento.js
@@ -2,8 +2,24 @@
 
 module.exports = function(Descuento) {
   // Ante la creacion/modificacion de descuentos.
+  var app = require('../../server/server');
   Descuento.on('changed', function(inst) {
+    let Evento = app.models.Evento;
+    let suscripcion = app.models.suscripcion;
+    let notificacion = app.models.notificacion;
+    console.log('inst: "' + JSON.stringify(inst) + '"');
     console.log('El descuento %s sufrio cambios.', inst.descripcion);
+    Evento.scope('eventosConDescuento', {where: {_id: inst.eventoId}});
+    Evento.eventosConDescuento(function(err, data) {
+      console.log(JSON.stringify(data));
+      suscripcion.scope('suscripcionAlEvento', {where: {eventoId: data._id}});
+      suscripcion.suscripcionAlEvento(function(err, data) {
+        console.log(JSON.stringify(data));
+        // ACA CREAMOS LA NOTIFICACION!
+        // notificacion.create({})
+      });
+      // console.log(JSON.stringify(Evento.nestRemoting('suscripcion')));
+    });
     // => model with id 1 has been changed
   });
   // Ante la eliminación de descuentos.
