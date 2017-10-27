94e4eb9f14d5a39491f43b7af9ad976ec23a7871
Nahuel <alvarez.nahuel@gmail.com>
2017-10-27 01:28:04 -0300

    Creacion de eventos sobre el modelo descuentos y middleware de prueba

--- common/models/descuento.js
+++ common/models/descuento.js
@@ -1,5 +1,8 @@
 'use strict';
 
 module.exports = function(Descuento) {
-
+  Descuento.on('changed', function(inst) {
+    console.log('Descuento with id %s has been changed', inst.id);
+    // => model with id 1 has been changed
+  });
 };
