2b20a6474042ef25fdc0977fd89eb5e98715dc04
Nahuel <alvarez.nahuel@gmail.com>
2017-10-27 12:15:27 -0300

    Se incorpora el alta de novedades por cambios sobre los descuentos de un espectaculo.

--- common/models/descuento.js
+++ common/models/descuento.js
@@ -3,28 +3,64 @@
 module.exports = function(Descuento) {
   // Ante la creacion/modificacion de descuentos.
   var app = require('../../server/server');
-  Descuento.on('changed', function(inst) {
+  Descuento.on('changed', function(dataDescuento) {
     let Evento = app.models.Evento;
     let suscripcion = app.models.suscripcion;
     let notificacion = app.models.notificacion;
-    console.log('inst: "' + JSON.stringify(inst) + '"');
-    console.log('El descuento %s sufrio cambios.', inst.descripcion);
-    Evento.scope('eventosConDescuento', {where: {_id: inst.eventoId}});
-    Evento.eventosConDescuento(function(err, data) {
-      console.log(JSON.stringify(data));
-      suscripcion.scope('suscripcionAlEvento', {where: {eventoId: data._id}});
-      suscripcion.suscripcionAlEvento(function(err, data) {
-        console.log(JSON.stringify(data));
-        // ACA CREAMOS LA NOTIFICACION!
-        // notificacion.create({})
+    console.log('inst: "' + JSON.stringify(dataDescuento) + '"');
+    console.log('El descuento %s sufrio cambios.', dataDescuento.descripcion);
+    Evento.scope('eventosConDescuento', {where: {_id: dataDescuento.eventoId}});
+    Evento.eventosConDescuento(function(err, dataEventos) {
+      console.log(JSON.stringify(dataEventos));
+      dataEventos.forEach(function(dataEvento) {
+        suscripcion.scope('suscripcionAlEvento'
+        , {where: {eventoId: dataEvento._id}});
+        suscripcion.suscripcionAlEvento(function(err, dataSuscripciones) {
+          dataSuscripciones.forEach(function(dataSuscripcion) {
+            console.log(JSON.stringify(dataSuscripcion));
+            // ACA CREAMOS LA NOTIFICACION!
+            let aviso = {
+              'tipo': 'mail',
+              'texto': 'Hoy novedades sobre el ' + dataDescuento.descripcion +
+              ' del evento ' + dataEvento.nombre,
+              'mail': dataSuscripcion.email};
+            notificacion.create(aviso);
+          });
+        });
+      });
+    });
+  });
+  // Ante la eliminación de descuentos.
+  /* Descuento.on('deleted', function(dataDescuento) {
+    console.log('El descuento %s ha sido dado de baja.'
+      , dataDescuento.descripcion);
+    // => model with id 1 has been changed
+    let Evento = app.models.Evento;
+    let suscripcion = app.models.suscripcion;
+    let notificacion = app.models.notificacion;
+    console.log('inst: "' + JSON.stringify(dataDescuento) + '"');
+    console.log('El descuento %s sufrio cambios.', dataDescuento.descripcion);
+    Evento.scope('eventosConDescuento', {where: {_id: dataDescuento.eventoId}});
+    Evento.eventosConDescuento(function(err, dataEventos) {
+      console.log(JSON.stringify(dataEventos));
+      dataEventos.forEach(function(dataEvento) {
+        suscripcion.scope('suscripcionAlEvento'
+        , {where: {eventoId: dataEvento._id}});
+        suscripcion.suscripcionAlEvento(function(err, dataSuscripciones) {
+          dataSuscripciones.forEach(function(dataSuscripcion) {
+            console.log(JSON.stringify(dataSuscripcion));
+            // ACA CREAMOS LA NOTIFICACION!
+            let aviso = {
+              'tipo': 'mail',
+              'texto': 'Se cancelo el ' + dataDescuento.descripcion +
+              ' del evento ' + dataEvento.nombre,
+              'mail': dataSuscripcion.email};
+            notificacion.create(aviso);
+          });
+        });
       });
       // console.log(JSON.stringify(Evento.nestRemoting('suscripcion')));
     });
-    // => model with id 1 has been changed
   });
-  // Ante la eliminación de descuentos.
-  Descuento.on('deleted', function(inst) {
-    console.log('El descuento %s ha sido dado de baja.', inst.descripcion);
-    // => model with id 1 has been changed
-  });
+*/
 };
