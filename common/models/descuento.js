ac3d0ec954f141f45fabf908f0a582d647e0d7a5
Nahuel <alvarez.nahuel@gmail.com>
2017-10-27 01:36:53 -0300

    Se amplia la gestion de eventos.

--- common/models/descuento.js
+++ common/models/descuento.js
@@ -1,8 +1,14 @@
 'use strict';
 
 module.exports = function(Descuento) {
+  // Ante la creacion/modificacion de descuentos.
   Descuento.on('changed', function(inst) {
-    console.log('Descuento with id %s has been changed', inst.id);
+    console.log('El descuento %s sufrio cambios.', inst.descripcion);
+    // => model with id 1 has been changed
+  });
+  // Ante la eliminación de descuentos.
+  Descuento.on('deleted', function(inst) {
+    console.log('El descuento %s ha sido dado de baja.', inst.descripcion);
     // => model with id 1 has been changed
   });
 };
